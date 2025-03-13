a455a6b0cd6bfc1ce119697964e65613c6b220e9
HazemHatem <Hazem Hatem>
2025-03-13 17:22:48 +0200

    auth

--- ECommerceBackEnd/app/Http/Controllers/API/Auth/AuthController.php
+++ ECommerceBackEnd/app/Http/Controllers/API/Auth/AuthController.php
@@ -4,64 +4,97 @@ namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
-use App\Http\Requests\API\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
 use App\Http\Requests\API\Auth\RegisterRequest;
+use App\Http\Requests\API\Auth\LoginRequest;
 
 class AuthController extends Controller
 {
-    //
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
-    }
 
-    public function login(LoginRequest $request)
-    {
-        $credentials = $request->validated();
+    /**
+     * Log in and obtain a JWT token.
+     *
+     * @param \App\Http\Requests\API\Auth\LoginRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
 
-        if (! $token = Auth::attempt($credentials)) {
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $validator = $request->validated();
+        if (! $token = auth('api')->attempt($validator)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
         return $this->respondWithToken($token);
     }
 
-    public function register(RegisterRequest $request)
-    {
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+    /**
+     * Register a new user.
+     *
+     * @param \App\Http\Requests\API\Auth\RegisterRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
 
         $token = JWTAuth::fromUser($user);
 
         return response()->json([
-            'message' => 'User successfully registered',
+            'message' => 'user created successfully',
             'user' => $user,
             'token' => $token,
         ], 200);
     }
 
 
-    public function logout()
-    {
-        auth()->logout();
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
 
-        return response()->json(['message' => 'Successfully logged out']);
+    public function getAccount(): JsonResponse
+    {
+        return response()->json(auth('api')->user());
     }
 
-    protected function respondWithToken($token)
+    /**
+     * Log out the authenticated user by invalidating their JWT token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function logout(): JsonResponse
+    {
+        auth('api')->logout();
+
+        return response()->json(['message' => 'user logged out successfully']);
+    }
+
+
+    /**
+     * Respond with a JSON containing the token details.
+     *
+     * @param string $token The JWT access token.
+     *
+     * @return \Illuminate\Http\JsonResponse A JSON response containing the access token, token type, and expiration time.
+     */
+
+    protected function respondWithToken($token): JsonResponse
     {
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => config('jwt.ttl') * 60
+            'expires_in' => JWTAuth::factory()->getTTL() * 60
         ]);
     }
 }
