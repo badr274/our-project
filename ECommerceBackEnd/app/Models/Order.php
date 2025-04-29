813b6d01fb2f9a09569e0989fdc16c525dc6a9d7
HazemHatem <Hazem Hatem>
2025-04-29 18:13:49 +0300

    Part From Database

--- ECommerceBackEnd/app/Models/Order.php
+++ ECommerceBackEnd/app/Models/Order.php
@@ -5,23 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Wishlist extends Model
+class Order extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'user_id',
-        'product_id',
+        'status',
+        'total_price',
     ];
 
-    public function product(): BelongsTo
-    {
-        return $this->belongsTo(Product::class);
-    }
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class)
+            ->withPivot('quantity', 'price_at_order')
+            ->withTimestamps();
     }
 }
